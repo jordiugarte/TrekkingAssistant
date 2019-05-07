cbc90986b123976902c2595854216068da9fe86c
jordiugarte <jordi933@gmail.com>
2019-05-07 08:27:35 -0400

    Structure change

--- app/src/main/java/com/galacticCat/chatbleu/MainActivity.java
+++ app/src/main/java/com/galacticCat/chatbleu/MainActivity.java
@@ -153,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
         listsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Mochila.class);
+                Intent intent = new Intent(MainActivity.this, MochilaActivity.class);
                 startActivity(intent); }
         });
         //Mapas
--- app/src/main/java/com/galacticCat/chatbleu/MochilaActivity.java
+++ app/src/main/java/com/galacticCat/chatbleu/MochilaActivity.java
@@ -1,24 +1,15 @@
 package com.galacticCat.chatbleu;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.galacticCat.chatbleu.ItemList;
-import com.galacticCat.chatbleu.R;
-
 import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
 
 public class MochilaActivity extends AppCompatActivity {
 
