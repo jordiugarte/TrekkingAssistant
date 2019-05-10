004a4c8a4e76def3c84340e1507fb344c9accd7b
ilizarazu18 <lizarazuaramayoignacio@gmail.com>
2019-05-10 13:13:42 -0400

    Lista de contactos con popup

--- app/src/main/java/com/galacticCat/chatbleu/ContactActivity.java
+++ app/src/main/java/com/galacticCat/chatbleu/ContactActivity.java
@@ -7,17 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.galacticCat.chatbleu.adapter.ContactAdapter;
 import com.galacticCat.chatbleu.model.Contact;
 
 import java.util.ArrayList;
 
-public class ContactActivity extends AppCompatActivity implements ContactEditor.ExampleDialogListener{
+public class ContactActivity extends AppCompatActivity implements ContactEditor.ExampleDialogListener {
 
     private ListView lvItem;
     private ContactAdapter adaptador;
     private ArrayList<Contact> listItems;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     private int currentPosition;
 
@@ -25,11 +29,12 @@ public class ContactActivity extends AppCompatActivity implements ContactEditor.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        sharedPreferences = getApplicationContext().getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
         lvItem = findViewById(R.id.lvItem);
         GetArrayItems();
         adaptador = new ContactAdapter(this, listItems);
         lvItem.setAdapter(adaptador);
-
         lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -45,15 +50,13 @@ public class ContactActivity extends AppCompatActivity implements ContactEditor.
                 ContactEditor contactEditor = new ContactEditor();
                 contactEditor.show(getSupportFragmentManager(), "example dialog");
                 currentPosition = position;
+                Toast.makeText(getApplicationContext(), currentPosition+"", Toast.LENGTH_LONG).show();
             }
         });
     }
 
     private void GetArrayItems() {
         listItems = new ArrayList<>();
-
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
 
         String[] nombre = new String[5];
         String[] numero = new String[5];
@@ -81,5 +84,12 @@ public class ContactActivity extends AppCompatActivity implements ContactEditor.
         listItems.set(currentPosition, new Contact(nombre, numero));
         adaptador = new ContactAdapter(this, listItems);
         lvItem.setAdapter(adaptador);
+
+        String keyContact = "C" + (currentPosition + 1) + "";
+        String keyNumber = "N" + (currentPosition + 1) + "";
+        Toast.makeText(getApplicationContext(), keyContact + " " + keyNumber, Toast.LENGTH_LONG).show();
+        editor.putString(keyContact, nombre);
+        editor.putString(keyNumber, numero);
+        editor.commit();
     }
 }
